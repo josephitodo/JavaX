aeef675fd7ae7e5388ffd18a883f188208e0cd13
Joseph Itodo <110479183+josephitodo@users.noreply.github.com>
2026-07-01 19:04:51 +0100

    Create projectGenerator.js

---
+++ src/projectGenerator.js
@@ -0,0 +1,21 @@
+export async function createJavaProject(rootPath, projectName, packageName) {
+
+    const fs = acode.require("fs");
+
+    const root = fs(`${rootPath}/${projectName}`);
+
+    // create project root
+    if (!(await root.exists())) {
+        await root.createDirectory(projectName);
+    }
+
+    const project = fs(`${rootPath}/${projectName}`);
+
+    await project.createDirectory("src");
+    await project.createDirectory("README.md"); // temporary test
+
+    acode.alert(
+        "JavaX",
+        `Project ${projectName} generated successfully.`
+    );
+}

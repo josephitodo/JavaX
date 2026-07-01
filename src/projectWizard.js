61819e65d697631b9874d7094656650dc14dccad
Joseph Itodo <110479183+josephitodo@users.noreply.github.com>
2026-07-01 18:41:35 +0100

    Update projectWizard.js

--- src/projectWizard.js
+++ src/projectWizard.js
@@ -2,21 +2,9 @@ export function showProjectWizard(type){
 
     const projectName = prompt("Project Name");
 
-    if(!projectName) return;
-
-    const packageName = prompt(
-        "Package Name",
-        "com.example"
-    );
-
     alert(
-`Creating ${type} project...
-
-Name:
-${projectName}
-
-Package:
-${packageName}`
+        "Project = " + projectName +
+        "\nType = " + type
     );
 
 }

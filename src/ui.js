0013052d2ef5ce8ecfbf0556497052c766736f97
Joseph Itodo <110479183+josephitodo@users.noreply.github.com>
2026-07-01 18:40:31 +0100

    Refactor showHome function to handle actions

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,8 @@
+import { showProjectWizard } from "./projectWizard";
+
 export async function showHome() {
 
-    const result = await acode.select(
+    const action = await acode.select(
         "JavaX",
         [
             "☕ New Java Project",
@@ -11,6 +13,27 @@ export async function showHome() {
         ]
     );
 
-    alert(JSON.stringify(result));
+    switch(action){
+
+        case "☕ New Java Project":
+            showProjectWizard("java");
+            break;
+
+        case "⚙ New Gradle Project":
+            showProjectWizard("gradle");
+            break;
+
+        case "📦 New Maven Project":
+            showProjectWizard("maven");
+            break;
+
+        case "📂 Open Existing Project":
+            alert("Coming Soon");
+            break;
+
+        case "⚙ Settings":
+            alert("Coming Soon");
+            break;
+    }
 
 }

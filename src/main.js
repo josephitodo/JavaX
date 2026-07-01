9cb9a959f28fe76b95a030263a0dd8ac329039b7
Joseph Itodo <110479183+josephitodo@users.noreply.github.com>
2026-07-01 17:35:18 +0100

    Refactor AcodePlugin to JavaX with initialization updates

--- src/main.js
+++ src/main.js
@@ -1,26 +1,49 @@
-import plugin from '../plugin.json';
+import plugin from "../plugin.json";
 
-class AcodePlugin {
+class JavaX {
 
   async init() {
-    // plugin initialisation 
+
+    console.log("JavaX v0.1.0 Loaded");
+
+    if (window.acode) {
+      acode.alert(
+        "JavaX",
+        "JavaX Plugin Loaded Successfully!"
+      );
+    }
+
   }
 
   async destroy() {
-    // plugin clean up
+
+    console.log("JavaX Unloaded");
+
   }
+
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
+
+  const javax = new JavaX();
+
+  acode.setPluginInit(
+    plugin.id,
+    async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+
+      if (!baseUrl.endsWith("/"))
+        baseUrl += "/";
+
+      javax.baseUrl = baseUrl;
+
+      await javax.init($page, cacheFile, cacheFileUrl);
+
     }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
-  });
-  acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
-  });
+  );
+
+  acode.setPluginUnmount(
+    plugin.id,
+    () => javax.destroy()
+  );
+
 }

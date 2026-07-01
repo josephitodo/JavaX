d14322ac6cd440e6e3345f89629a15912d6fa98e
Joseph Itodo <110479183+josephitodo@users.noreply.github.com>
2026-07-01 18:50:22 +0100

    Update projectWizard.js

--- src/projectWizard.js
+++ src/projectWizard.js
@@ -1,10 +1,41 @@
-export function showProjectWizard(type){
+export async function showProjectWizard(type){
 
     const projectName = prompt("Project Name");
 
+    if(!projectName) return;
+
+    const packageName = prompt(
+        "Package Name",
+        "com.example"
+    );
+
+    if(!packageName) return;
+
+    const javaVersion = await acode.select(
+        "Java Version",
+        [
+            "Java 8",
+            "Java 11",
+            "Java 17",
+            "Java 21"
+        ]
+    );
+
     alert(
-        "Project = " + projectName +
-        "\nType = " + type
+
+`Creating ${type} project
+
+Project:
+${projectName}
+
+Package:
+${packageName}
+
+Java:
+${javaVersion}
+
+(Next version will generate the project.)`
+
     );
 
 }
